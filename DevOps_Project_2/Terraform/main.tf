1ba1a59df1c003757343619e3ae46e2ae6430fb3
frontechsu <99862754+frontechsu@users.noreply.github.com>
2022-12-06 15:08:47 +0530

    Update main.tf

--- DevOps_Project_2/Terraform/main.tf
+++ DevOps_Project_2/Terraform/main.tf
@@ -1,17 +1,3 @@
-data "aws_ami" "amazon-linux" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-kernel-5.10-hvm-*-x86_64-gp2"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-}
-
 resource "aws_instance" "dev_machine" {
   ami = ami-0f5e8a042c8bfcd5e
   instance_type = "t2.micro"
